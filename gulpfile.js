d1c28acd5b5fbbb68186eddb82837c8e78721bb3
Keyla Juleyma Cardenas Esteban <kjcardenase@pop-os.localdomain>
2022-05-31 10:19:39 -0500

    terminando-proceso

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+
+//CSS Y SASS
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+//IMAGENES
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+function css(done) {
+    //compilar sass
+    //pasos: 1 - identificar archivo, 2 - compilarla, 3 - guardar el .css
+    src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(dest('build/css'));
+
+    done();
+}
+
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe(imagemin({ optimizationlevel: 3 }))
+        .pipe(dest('build/img'));
+}
+
+function versionwebp() {
+    const opciones = {
+        quality: 50,
+    };
+
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+}
+
+function versionavif() {
+    const opciones = {
+        quality: 50,
+    };
+
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'));
+}
+
+function dev() {
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
+}
+
+module.exports.css = css;
+module.exports.dev = dev;
+module.exports.imagenes = imagenes;
+module.exports.versionwebp = versionwebp;
+module.exports.versionavif = versionavif;
+module.exports.default = series(imagenes, versionwebp, versionavif, css, dev);
+
+//series - se inicia una tarea y hasta qu efinaliza
+//parallel - todas inician al mismo tiempo
